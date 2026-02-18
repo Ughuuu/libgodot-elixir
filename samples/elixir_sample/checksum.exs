76127e28826cd3e5c2570ee14457e818c6366ee3
Dragos Daian <daian.dragos@yahoo.com>
2026-02-18 09:31:08 +0100

    update to 4.5.1-5

--- samples/elixir_sample/checksum.exs
+++ samples/elixir_sample/checksum.exs
@@ -1,3 +1,5 @@
 %{
-  "lib_godot_connector-nif-2.17-aarch64-apple-darwin-4.5.1-4.tar.gz" => "sha256:90283cbad67cda96a057a147fea3d0f654fdbe6264b21bf3777f534ce8c6cc47",
+  "lib_godot_connector-nif-2.17-aarch64-apple-darwin-4.5.1-5.tar.gz" => "sha256:e56ace0be38005f86954a906e43e14647d2a620c6a9de95346ef877f8bc96b8e",
+  "lib_godot_connector-nif-2.17-x86_64-linux-gnu-4.5.1-5.tar.gz" => "sha256:daeb6bbdf1d895c82b2d1e02f1b5e8c63cd5678e1046e28b59708f26ed1fb7f4",
+  "lib_godot_connector-nif-2.17-x86_64-pc-windows-4.5.1-5.tar.gz" => "sha256:d9790e42dd6a752caddd05d8eece60399c3b82785463a23ef837c56729f30e34",
 }
